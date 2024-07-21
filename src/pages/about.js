973b98d1a412dd8d1207aeb5f4d79fb74892be2a
tomas-purplefire <tomas@purplefire.io>
2024-07-21 13:37:30 -0300

    new structure

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,12 +1,13 @@
 import * as React from "react";
 import { Link } from "gatsby";
 
-export default function IndexPage() {
+export default function AboutPage() {
   return (
     <>
-      <h1>Welcome to my beautiful page suckas</h1>
-      <Link to="about"></Link>
+      <h1>Welcome to the about page</h1>
+      <Link to="/">Go back to Home</Link>
     </>
   );
 }
+
 export const Head = () => <title>Home Page</title>;
